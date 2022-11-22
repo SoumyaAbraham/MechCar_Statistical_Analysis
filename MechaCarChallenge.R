8afb030c59a9c4583b2e134101c6f9b017cb87c2
SoumyaAbraham <110578540+SoumyaAbraham@users.noreply.github.com>
2022-11-22 07:12:16 -0600

    del 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+
 # DELIVERABLE 1
 
 # load dplyr package
@@ -33,4 +34,26 @@ lot_summary<- suspCoil_data %>% group_by(Manufacturing_Lot) %>% summarize (Mean_
                                                                           Variance_PSI= var(PSI),
                                                                           StdDev_PSI= sd(PSI),
                                                                           Num_Coil=n(), .groups = 'keep')
+
+# DELIVERABLE 3
+
+# box plot for PSI for all lots
+plt1<- ggplot(suspCoil_data, aes(y=PSI))
+plt1 + geom_boxplot()
+
+# Use t.test() to determine if the PSI across all lots is statistically different from population mean of 1,500 PSI
+t.test(suspCoil_data$PSI, mu=1500)
+
+# box plot for PSI for each lot
+plt2<- ggplot(suspCoil_data, aes(x=Manufacturing_Lot, y= PSI))
+plt2 + geom_boxplot()
+
+# Use t.test to determine if the PSI for each lot is statistically different from population mean of 1,500 PSI
+lot1<- subset(suspCoil_data, Manufacturing_Lot == 'Lot1')
+lot2<- subset(suspCoil_data, Manufacturing_Lot == 'Lot2')
+lot3<- subset(suspCoil_data, Manufacturing_Lot == 'Lot3')
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
 
